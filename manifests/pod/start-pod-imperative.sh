fc1f239ebfaccb697006bc0707ad6cf0f69ff315
eumirandarainer <rainer.lopes@gmail.com>
2023-03-29 15:28:07 -0300

    Update at Wed 29 Mar 2023 03:28:07 PM -03

--- manifests/pod/start-pod-imperative.sh
+++ manifests/pod/start-pod-imperative.sh
@@ -2,11 +2,11 @@
 
 # Script para iniciar um pod de maneira "imperativa"
 
-kubectl run sample-app-imperative --image=eumirandarainer/sample-app --port=8888 --labels app=sample-app
+kubectl run sample-app-imperative --image=eumirandarainer/simple-go-app --port=8888 --labels app=simple-go-app
 
 # Fazer o port-forwarding para que as conexoes sejam direcionadas corretamente
 
-kubectl port-forward pod/sample-app-imperative 9999:8888
+kubectl port-forward pod/simple-go-app-imperative 9999:8888
 
 # Remove o Pod
-kubectl delete pod sample-app-imperative
+kubectl delete pod simple-go-app-imperative
